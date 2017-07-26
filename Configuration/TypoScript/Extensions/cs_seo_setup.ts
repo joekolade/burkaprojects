c31310021ed9dcb25bf435670e84fd2818ab89c2
Joe Schäfer <schaefer@scw.info>
2017-07-26 15:00:59 +0200

    custom categories

--- Configuration/TypoScript/Extensions/cs_seo_setup.ts
+++ Configuration/TypoScript/Extensions/cs_seo_setup.ts
@@ -9,33 +9,33 @@ plugin.tx_csseo.sitemap {
             additionalParams = tx_burkaprojects_projectlist[project]
 
             # string; add custom query options : title LIKE '%top%'
-            # additionalWhereClause =
+            additionalWhereClause =
 
             # comma separated list; page uid where are the records stored
-            storagePid = 34
+            #storagePid = 34
             storagePid = 2
 
             # int; page uid where the details are shown
-            detailPid = 35
+            #detailPid = 35
             detailPid = 3
 
             # comma separated list; configure the available languages, e.g.: 0,1
-            # languageUids =
+            languageUids =
 
             # comma separated list; display only records with this categories
-            categories = 1
+            categories = 1,2,3,4
 
             # string; if categories were saved in the same table, set here the field name
-            # categoryField = categories
+            categoryField =
 
             # string; if categories were saved in a MM-Table set here the name of the table
-            categoryMMTable = sys_category_record_mm
+            categoryMMTable = tx_burkaprojects_project_category_mm
 
             # boolean; true if the MM table has a column 'tablenames' which contains the current record table
-            #categoryMMTablenames = 1
+            categoryMMTablenames = 0
 
             # string; if set the column 'fieldname' of the MM-table must be this
-            #categoryMMFieldname = categories
+            categoryMMFieldname =
         }
     }
 }
